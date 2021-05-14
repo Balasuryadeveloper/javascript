f5c8dfbc6d19bc42442630f3fc26f31207f75cec
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-05-14 09:12:32 +0530

    file was modified

--- sample.js
+++ sample.js
@@ -1,7 +1,19 @@
+//printing statement
+console.log("This is javascript program");
+//variable declaration
+var n1=10;
+let n2=12;
+const n3=2;
+console.log(n1);
+console.log(n2);
+console.log(n3);
 //sample program
 var a,b,c;
 a=10;
 b=2;
 c=a+b;
 console.log(c);
+//find the variable type
+var a=1;
+console.log(typeof(a));
 
